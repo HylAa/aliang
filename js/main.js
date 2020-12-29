b291e52fb3b7fc61be7959606b818a43c283b193
aliang <501747372@qq.com>
2020-12-29 16:10:59 +0800

    Site updated: 2020-12-29 16:10:59

--- js/main.js
+++ js/main.js
@@ -247,38 +247,40 @@ document.addEventListener('DOMContentLoaded', function () {
  * fancybox和 mediumZoom
  */
   const addFancybox = function (ele) {
-    const runFancybox = (ele) => {
-      ele.each(function (i, o) {
-        const $this = $(o)
-        const lazyloadSrc = $this.attr('data-lazy-src') || $this.attr('src')
-        const dataCaption = $this.attr('alt') || ''
-        $this.wrap(`<a href="${lazyloadSrc}" data-fancybox="group" data-caption="${dataCaption}" class="fancybox"></a>`)
-      })
+    if (ele.length) {
+      const runFancybox = (ele) => {
+        ele.each(function (i, o) {
+          const $this = $(o)
+          const lazyloadSrc = $this.attr('data-lazy-src') || $this.attr('src')
+          const dataCaption = $this.attr('alt') || ''
+          $this.wrap(`<a href="${lazyloadSrc}" data-fancybox="group" data-caption="${dataCaption}" class="fancybox"></a>`)
+        })
 
-      $().fancybox({
-        selector: '[data-fancybox]',
-        loop: true,
-        transitionEffect: 'slide',
-        protect: true,
-        buttons: ['slideShow', 'fullScreen', 'thumbs', 'close'],
-        hash: false
-      })
-    }
+        $().fancybox({
+          selector: '[data-fancybox]',
+          loop: true,
+          transitionEffect: 'slide',
+          protect: true,
+          buttons: ['slideShow', 'fullScreen', 'thumbs', 'close'],
+          hash: false
+        })
+      }
 
-    if (typeof $.fancybox === 'undefined') {
-      $('head').append(`<link rel="stylesheet" type="text/css" href="${GLOBAL_CONFIG.source.fancybox.css}">`)
-      $.getScript(`${GLOBAL_CONFIG.source.fancybox.js}`, function () {
+      if (typeof $.fancybox === 'undefined') {
+        $('head').append(`<link rel="stylesheet" type="text/css" href="${GLOBAL_CONFIG.source.fancybox.css}">`)
+        $.getScript(`${GLOBAL_CONFIG.source.fancybox.js}`, function () {
+          runFancybox($(ele))
+        })
+      } else {
         runFancybox($(ele))
-      })
-    } else {
-      runFancybox($(ele))
+      }
     }
   }
 
   const addMediumZoom = () => {
     const zoom = mediumZoom(document.querySelectorAll('#article-container :not(a)>img'))
     zoom.on('open', e => {
-      const photoBg = document.documentElement.getAttribute('data-theme') === 'dark' ? '#121212' : '#fff'
+      const photoBg = $(document.documentElement).attr('data-theme') === 'dark' ? '#121212' : '#fff'
       zoom.update({
         background: photoBg
       })
@@ -286,17 +288,15 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   const jqLoadAndRun = () => {
-    const $fancyboxEle = GLOBAL_CONFIG.lightbox === 'fancybox'
-      ? document.querySelectorAll('#article-container :not(a):not(.gallery-group) > img, #article-container > img')
-      : []
-    const fbLengthNoZero = $fancyboxEle.length > 0
+    const isFancybox = GLOBAL_CONFIG.lightbox === 'fancybox'
+    const $fancyboxEle = isFancybox ? document.querySelectorAll('#article-container :not(a):not(.gallery-group) > img, #article-container > img') : null
     const $jgEle = document.querySelectorAll('#article-container .justified-gallery')
-    const jgLengthNoZero = $jgEle.length > 0
+    const jgEleLength = $jgEle.length
 
-    if (jgLengthNoZero || fbLengthNoZero) {
+    if (jgEleLength || $fancyboxEle !== null) {
       btf.isJqueryLoad(() => {
-        jgLengthNoZero && runJustifiedGallery($jgEle)
-        fbLengthNoZero && addFancybox($fancyboxEle)
+        jgEleLength && runJustifiedGallery($jgEle)
+        isFancybox && addFancybox($fancyboxEle)
       })
     }
   }
